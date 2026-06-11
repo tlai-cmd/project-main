502d5a82a5f8488cbb802f8232202834c4f2f186
tlai-cmd <tlai@students.mags.school.nz>
2026-06-12 09:36:11 +1200

    spawner + enemy
    Finishing the enemy path and spawner

--- project-main/scenes/path_spawner.gd
+++ project-main/scenes/path_spawner.gd
@@ -3,8 +3,15 @@ extends Path2D
 @export var timer: Timer
 @export var enemy: PackedScene
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	pass
+	
+func _on_spawner_timeout() -> void:
 	var enemy_scene = enemy.instantiate()
-	if timer.timeout:
-		add_child(enemy_scene)
+	add_child(enemy_scene)
+
+
+func _disappear(area: Area2D) -> void:
+	queue_free()
