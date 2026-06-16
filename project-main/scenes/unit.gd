1ecdc442e2dde958007acd6d1d94af5cacea143d
tlai-cmd <tlai@students.mags.school.nz>
2026-06-16 15:07:39 +1200

    change the function

--- project-main/scenes/unit.gd
+++ project-main/scenes/unit.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 var enemy: CharacterBody2D
 var enemies = [ ]
+var closest_enemy: CharacterBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,16 +11,25 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if not closest_enemy == null:
+		look_at(closest_enemy.global_position)
 	
 
 
 func _body_entered(body: Node2D) -> void:
 	if body.is_in_group("enemy"):
 		look_at(enemy.global_position)
+		closest_enemy = body
 		enemies.append(body)
+		
+		
+#for node in enemies:
+#if node.get_parent().progress_ratio > closest_enemy.get_parent().progress_ratio:
+#closest_enemy == node
+		
 
 
 func _body_exited(body: Node2D) -> void:
 	if body.is_in_group("enemy"):
 		enemies.erase(body)
+		closest_enemy = null
