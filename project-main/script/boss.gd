329b512d9ab5b3c94ad4b362fd6a034648197476
tlai-cmd <tlai@students.mags.school.nz>
2026-07-20 14:27:08 +1200

    boss update p2
    boss health ui added

--- project-main/script/boss.gd
+++ project-main/script/boss.gd
@@ -2,10 +2,14 @@ extends CharacterBody2D
 var health: int = 5
 var level: Node2D
 
+@export var health_ui: ProgressBar
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	for node in get_tree().get_nodes_in_group("level"):
 		level = node
+	health_ui.max_value = health
+	health_ui.value = health
 
 
 
@@ -16,6 +20,7 @@ func _process(delta: float) -> void:
 func take_damage() -> void:
 	if health > 0:
 		health -= 1
+		health_ui.value = health
 	else:
 		queue_free()
 		level.money += 4
